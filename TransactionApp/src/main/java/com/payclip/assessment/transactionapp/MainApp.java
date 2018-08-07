74f8b0b27afabe05540a63eef4af922114a0d505
t_rodrc <carlos.r@MX-IT00151.globant.com>
2018-08-07 10:57:46 -0500

    initial commit

---
+++ TransactionApp/src/main/java/com/payclip/assessment/transactionapp/MainApp.java
@@ -0,0 +1,9 @@
+package com.payclip.assessment.transactionapp;
+
+public class MainApp {
+
+    public static void main(String[] args) {
+        System.out.println("com.payclip.assessment.transactionapp.MainApp.main()");
+    }
+    
+}
