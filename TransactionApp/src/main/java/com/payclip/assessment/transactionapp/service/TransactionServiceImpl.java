d5a7da2b4eac4c5efc0259b1f419ad233cdd637a
t_rodrc <carlos.r@MX-IT00151.globant.com>
2018-08-07 11:57:26 -0500

    set up options menu from service

--- TransactionApp/src/main/java/com/payclip/assessment/transactionapp/service/TransactionServiceImpl.java
+++ TransactionApp/src/main/java/com/payclip/assessment/transactionapp/service/TransactionServiceImpl.java
@@ -9,7 +9,15 @@ public class TransactionServiceImpl implements TransactionService {
     public void processingTransaction(String arguments[]) {
         if(arguments.length > 1) {
             if(arguments[0] != null && arguments[1] != null)
-                System.out.println("Args "+ arguments[0] + " / " + arguments[1]);
+                if(arguments[1].equals("add")){
+                    System.out.println("ADD TRANSACTION: ./application <user_id> add <transaction_json>");
+                }else if(arguments[1].equals("list")){
+                    System.out.println("LIST TRANSACTIONS: ./application <user_id> list");
+                }else if(arguments[1].equals("sum")){
+                    System.out.println("SUM TRANSACTIONS: ./application <user_id> sum");
+                }else if(arguments.length < 3){
+                    System.out.println("SHOW TRANSACTION: ./application <user_id> <transaction_id>");
+                }
         } else {
             System.out.println("NO OPTION SELECTED");
         }
