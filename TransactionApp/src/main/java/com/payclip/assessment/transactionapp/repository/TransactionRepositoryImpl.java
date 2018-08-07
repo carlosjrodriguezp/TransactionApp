a447d1c97fcdcd2e5ce75d2f41a1b366bf6442ba
t_rodrc <carlos.r@MX-IT00151.globant.com>
2018-08-07 12:38:08 -0500

    implement FileWriter to save json data in file

--- TransactionApp/src/main/java/com/payclip/assessment/transactionapp/repository/TransactionRepositoryImpl.java
+++ TransactionApp/src/main/java/com/payclip/assessment/transactionapp/repository/TransactionRepositoryImpl.java
@@ -1,15 +1,30 @@
 package com.payclip.assessment.transactionapp.repository;
 
 import com.payclip.assessment.transactionapp.model.Transaction;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class TransactionRepositoryImpl implements TransactionRepository {
+    
+    private static final String FILE_NAME = "data-stored";
 
     @Override
     public Transaction save(Transaction transaction) {
-        System.out.println("TRANSACTION: " + transaction.toString());
+        try {
+            FileWriter fw = new FileWriter(FILE_NAME, true);
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.write(transaction.toString());
+            bw.newLine();
+            bw.close();
+        } catch (IOException ex) {
+            Logger.getLogger(TransactionRepositoryImpl.class.getName()).log(Level.SEVERE, null, ex);
+        }
         return transaction;
     }
 
